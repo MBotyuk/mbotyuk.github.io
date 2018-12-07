07c86b545bb42f16cc2f506ebc077944de20924f
MBotyuk <mbotyuk@gmail.com>
2018-12-07 23:49:45 +0300

    price complited

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-menu.onclick = function myFunction() {
-	var x = document.getElementById("myTopnav");
+function pas() 
+{
+	login_ok = false;
+	user_name = "";
+	user_name = prompt("Для отображения прайсов введите пароль.","");
+	user_name = user_name.toLowerCase();
 
-	if(x.className === "topnav"){
-		x.className += "responsive";
-	}else{
-		x.className = "topnav";
-	}
+		if (user_name == "1")
+		{
+ 			document.write("<style>.tabl{display: block;}</style>");
+		} else {
+			alert("Вы ввели неверный пароль!");
+			document.write("<style>.error_pass{display: block;}</style>");
+		}
 }
